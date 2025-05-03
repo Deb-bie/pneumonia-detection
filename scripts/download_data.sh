9f3de7514461c9e838e76deea2a678bd0ee85faa
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-05-03 12:07:32 -0500

    creating target directory when it doesn't exist

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -2,6 +2,8 @@
 
 TARGET_DIR="../data/raw"
 
+mkdir -p "$TARGET_DIR"
+
 curl -L -o "$TARGET_DIR/x-ray-pneumonia-data.zip"\
   https://www.kaggle.com/api/v1/datasets/download/jijivishaphuoc/x-ray-pneumonia-data
 
